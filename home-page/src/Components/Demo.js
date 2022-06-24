f515b7cd3ccae964ed4284a81d42cfcb0a54ad25
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-06-24 15:42:52 +0200

    Added demo page for testing
    - Added /demo section for testing

--- home-page/src/Components/Demo.js
+++ home-page/src/Components/Demo.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { useEffect } from 'react';
+import '../Css/Demo.css';
+
+const id = ''
+
+const apiUrl = 'https://api.spotify.com/v1/artists/{id}/top-tracks'
+
+const apiKey =  'BQDEDcHnXMjg9dtdSNwGB_t8NSJQY7JH_whH4Q66Fe5CbnPYjujkHhVjtFqZp1VyFwY5q68yl0Sk8AmmietaEfHZNVMAymttiUxCIQrzAucMPDJulL-cGSfDijUD6mgFJpWt_NfDTJKxb-tzn9Cmk7w8U6DqO4nFn-XktexFlvwK6A'
 
 export default function Demo() {
+
+   /*  useEffect(() => {
+        function fetchArtist();
+    }
+    , [])
+ */
     return (
         <div className="demo">
             <header className="demo-header">
-                <h1 className="demo-h1">DEMO</h1>
+                <h1 className="demo-h1">SPOTIFY DEMO</h1>
             </header>
             <section className="demo-section">
-                <h2>Spotify API DEMO</h2>
-                <div className="spotify-demo">
-                    
-                </div>
+                <h2>SeedTheEmcee</h2>
+                <iframe src="https://open.spotify.com/embed/artist/1i60jNx9tWYWk0maS1UDsL?utm_source=generator" />
             </section>
         </div>
     );
